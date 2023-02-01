919df11e0ce753134ebf8db4928b9a392be2bd95
PetiDominik <petidomi05@gmail.com>
2023-02-01 12:29:35 +0100

    potokOsszegzese metodus, eredmeny parameter megvaltoztatva

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # megoldás
-def eredmeny(jatekosPontja, gepPontja):
-    if jatekosPontja > 21:
+def eredmeny(jatekosPontjai: list[int], gepPontjai: list[int]) -> str:
+    if pontOsszeg(jatekosPontjai) > 21:
         return "Játékos veszített"
-    elif gepPontja > 21:
+    elif pontOsszeg(gepPontjai) > 21:
         return "Gép veszített"
 
+def pontOsszeg(lista: list[int]) -> int:
+    osszeg = 0
+    for ertek in lista:
+        osszeg += ertek
+    return osszeg
 # teszteset
